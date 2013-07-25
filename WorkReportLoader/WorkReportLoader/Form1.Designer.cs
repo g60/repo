44de9cc5988db2561d3f6f93c31929161793c907
David <davidletts@gmail.com>
2013-07-25 12:40:32 +0100

    Finished work report loader for end of day

--- WorkReportLoader/WorkReportLoader/Form1.Designer.cs
+++ WorkReportLoader/WorkReportLoader/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(86, 28);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(293, 267);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.txtFileName);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(285, 241);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(285, 241);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(279, 235);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clean";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 267);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WorkReportLoader/WorkReportLoader/Form1.cs
+++ WorkReportLoader/WorkReportLoader/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,220 @@ namespace WorkReportLoader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+
+            fdlg.Title = "Select file";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.FileName = txtFileName.Text;
+            fdlg.Filter = "Text and CSV Files(*.txt, *.csv)|*.txt;*.csv|Text Files(*.txt)|*.txt|CSV Files(*.csv)|*.csv|All Files(*.*)|*.*";
+            fdlg.FilterIndex = 1;
+            fdlg.RestoreDirectory = true;
+
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFileName.Text = fdlg.FileName;
+                Import(fdlg.FileName);
+                Application.DoEvents();
+            } // end if
+
+        }
+
+        DataTable dt_work = new DataTable();
+
+        private void Import(string filename)
+        {
+            int lineNo = 0;
+
+            dt_work.Clear();
+            dt_work.Columns.Clear();
+
+
+            TextFieldParser parser = new TextFieldParser(filename);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            while (!parser.EndOfData)
+            {
+                string[] fields = parser.ReadFields();
+
+                if (lineNo == 0)
+                {
+                    
+                    for (int i = 0; i < fields.Length; i++)
+                    {
+                        dt_work.Columns.Add(fields[i]);
+                    } // end for
+                }
+                else
+                {
+                    DataRow rowToAdd = dt_work.NewRow();
+
+                    rowToAdd.ItemArray = fields;
+
+                    //for (int i = 0; i < fields.Length; i++)
+                    //{
+                    //    rowToAdd.ItemArray[i] = fields[i];
+                    //} // end for
+                    
+                    dt_work.Rows.Add(rowToAdd);
+                } // end if-then-else
+
+                lineNo++;
+
+            }
+
+
+
+            parser.Close();
+
+            dataGridView1.DataSource = dt_work;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable new_dt_work = dt_work.Clone();
+
+            List<int> rowsToRemove = new List<int>();
+
+            string curr_tech = "";
+
+            // have to process the table backwards!!
+            for (int i = dt_work.Rows.Count-1; i >= 0; i--)
+            {
+
+                DataRow curr_row = dt_work.Rows[i];
+
+                // remove any blank rows
+                if (curr_row["Prefix"].ToString() == "")
+                {
+                    if (!rowsToRemove.Contains(i))
+                    {
+                        rowsToRemove.Add(i);
+                    } // end if
+                } // end if
+
+                // remove any total rows
+                if (String.Compare(curr_row["Prefix"].ToString(), "Totals", true) == 0)
+                {
+                    if (!rowsToRemove.Contains(i))
+                    {
+                        rowsToRemove.Add(i);
+                    } // end if
+                } // end if
+
+                // remove any grand total rows
+                if (String.Compare(curr_row["Prefix"].ToString(), "Grand Totals", true) == 0)
+                {
+                    if (!rowsToRemove.Contains(i))
+                    {
+                        rowsToRemove.Add(i);
+                    } // end if
+                } // end if
+
+                // fill in the technicians name
+                if (curr_row["TechInit"].ToString() == "")
+                {
+                    curr_row["TechInit"] = curr_tech;
+                }
+                else
+                {
+                    curr_tech = curr_row["TechInit"].ToString();
+                } // end if-then-else
+
+            } // end for
+
+            
+
+            foreach (int rowToRemove in rowsToRemove)
+            {
+                dt_work.Rows.RemoveAt(rowToRemove);
+            } // end foreach
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string CsvFpath = @"C:\scanner\CSV-EXPORT.csv";
+            try
+            {
+                System.IO.StreamWriter csvFileWriter = new StreamWriter(CsvFpath, false);
+
+                string columnHeaderText = "";
+
+                int countColumn = dt_work.Columns.Count;
+
+                columnHeaderText = dt_work.Columns[0].ColumnName;
+
+                for (int i = 1; i < countColumn; i++)
+                {
+                    columnHeaderText = columnHeaderText + ',' + dt_work.Columns[i].ColumnName;
+                }
+
+                csvFileWriter.WriteLine(columnHeaderText);
+
+                foreach (DataRow currRow in dt_work.Rows)
+                {
+                    for (int i = 0; i < currRow.ItemArray.Length; i++)
+                    {
+                        if (i > 0)
+                        {
+                            csvFileWriter.Write(",");
+                        } // end if
+
+                        csvFileWriter.Write("\"" + currRow.ItemArray[i] + "\"");
+                    } // end for
+
+                    csvFileWriter.WriteLine();
+
+                } // end foreach
+
+                //string columnHeaderText = "";
+
+                //int countColumn = dataGridView1.ColumnCount - 1;
+
+                //if (countColumn >= 0)
+                //{
+                //    columnHeaderText = dataGridView1.Columns[0].HeaderText;
+                //}
+
+                //for (int i = 1; i <= countColumn; i++)
+                //{
+                //    columnHeaderText = columnHeaderText + ',' + dataGridView1.Columns[i].HeaderText;
+                //}
+
+
+                //csvFileWriter.WriteLine(columnHeaderText);
+
+                //foreach (DataGridViewRow dataRowObject in dataGridView1.Rows)
+                //{
+                //    if (!dataRowObject.IsNewRow)
+                //    {
+                //        string dataFromGrid = "";
+
+                //        dataFromGrid = dataRowObject.Cells[0].Value.ToString();
+
+                //        for (int i = 1; i <= countColumn; i++)
+                //        {
+                //            dataFromGrid = dataFromGrid + ',' + dataRowObject.Cells[i].Value.ToString();
+
+                //            csvFileWriter.Write(dataFromGrid);
+                //        }
+                //        csvFileWriter.WriteLine();
+                //    }
+                //}
+
+
+                csvFileWriter.Flush();
+                csvFileWriter.Close();
+            }
+            catch (Exception exceptionObject)
+            {
+                MessageBox.Show(exceptionObject.ToString());
+            }
         }
     }
 }
